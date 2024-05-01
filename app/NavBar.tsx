d87ac8d76d0720a3cf77dc7c9182398696ae89dc
cmc1026 <127949422+cmc1026@users.noreply.github.com>
2024-05-01 16:20:57 -0400

    added Icon

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,10 +1,11 @@
 import Link from 'next/link'
 import React from 'react'
+import { RxActivityLog } from "react-icons/rx";
 
 const NavBar = () => {
   return (
     <nav className="flex space-x-6 mb-5 px-5 h-14 items-center border-b-2">
-      <Link href="/">Home</Link>
+      <Link href="/"><RxActivityLog/></Link>
       <Link href="../components/task/">Tasks</Link>
       <Link href="../components/addTask/">New Task</Link>
     </nav>
@@ -12,3 +13,4 @@ const NavBar = () => {
 }
 
 export default NavBar
+
