b82d78ad91aa3d3bc04e2abbd22ffba3df6803a4
cmc1026 <127949422+cmc1026@users.noreply.github.com>
2024-05-02 13:04:09 -0400

    Create the mySql Database

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -5,7 +5,7 @@ import { RxActivityLog } from "react-icons/rx";
 const NavBar = () => {
   return (
     <nav className="flex space-x-6 mb-5 px-5 h-14 items-center border-b-2">
-      <Link href="/"><RxActivityLog/></Link>
+      <Link href="/" ><RxActivityLog/></Link>
       <Link href="../components/task/">Tasks</Link>
       <Link href="../components/addTask/">New Task</Link>
     </nav>
---
+++ prisma/migrations/20240502170200_init/migration.sql
@@ -0,0 +1,24 @@
+-- CreateTable
+CREATE TABLE `User` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `email` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NULL,
+    `password` VARCHAR(191) NULL,
+
+    UNIQUE INDEX `User_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Task` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+    `desc` VARCHAR(191) NOT NULL,
+    `dueDate` DATETIME(3) NOT NULL,
+    `userId` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Task` ADD CONSTRAINT `Task_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
